4f78853942bd82ff13f702ebc954595313810db0
Mohamed Riaz <mohamed.riaz1307@gmail.com>
2021-02-23 20:24:30 +0530

    models registered in admin

--- backend_p1/library/urls.py
+++ backend_p1/library/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', RedirectView.as_view(url='home/', permanent=True)), #redirect home page to new page
-    path('home/', views.home),
+    # path('home/', views.home),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
